22cbe13420fd2466f5ea0967896704887f430c76
RandomDrone <robertl2c5@o2.pl>
2021-12-29 13:13:12 +0100

    czwarty comit - test gitignore, zmiana w app.js

--- app.js
+++ app.js
@@ -6,3 +6,8 @@
 
 //package.json - plik konfiguracyjny ->mozna stworzyc reczine albo npm init ; npm init -y
     
+const _  = require('lodash')
+
+const items = [1,[2,[3,[4]]]]
+const newItems = _.flattenDeep(items)
+console.log(newItems)
